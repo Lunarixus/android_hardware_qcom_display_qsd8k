e4fb5bb39dda51299d15ec9ebb96ee86ce50e9e2
Lunarixus <Lunarixus@raphielgang.org>
2020-02-11 00:44:39 +0000

    display: Add base for qsd8k support

--- Android.mk
+++ Android.mk
@@ -1,10 +1,9 @@
-ifeq ($(call my-dir)/$(TARGET_BOARD_PLATFORM),$(call project-path-for,qcom-display))
-
 LOCAL_PATH := $(call my-dir)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
 
-ifneq ($(filter msm8x84,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
+  include $(call all-named-subdir-makefiles,qsd8k)
+else ifneq ($(filter msm8x84,$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,msm8084)
 else ifneq ($(filter msm8x26,$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,msm8226)
@@ -12,8 +11,4 @@ else ifneq ($(filter msm8992,$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,msm8994)
 else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
-endif
-
-endif
-
 endif
